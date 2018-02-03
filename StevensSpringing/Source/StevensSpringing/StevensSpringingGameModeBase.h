89e5590006224405ae3bb1757846581a58ad33ed
Stefano Angelini - Desktop <kuroiraion@hotmail.it>
2018-02-03 20:30:39 +0100

    Initial Commit

---
+++ StevensSpringing/Source/StevensSpringing/StevensSpringingGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StevensSpringingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEVENSSPRINGING_API AStevensSpringingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
