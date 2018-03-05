7fcbf66a6516e8c6f504bd4185992c7b3a0bef20
Stefano Angelini - Surface <kuroiraion@hotmail.it>
2018-03-05 18:10:55 +0100

    lesson 81, now I know why bad shooters have collision on holes in geometry. I mean, duh, I had guessed already, after a lifetime of gaming

--- StevensSpringing/Source/StevensSpringing/Grabber.cpp
+++ StevensSpringing/Source/StevensSpringing/Grabber.cpp
@@ -36,7 +36,7 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
-	// get player viewpoint this tick
+	/// get player viewpoint this tick
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
@@ -50,7 +50,7 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	FVector GrabReach = PlayerViewPointRotation.Vector() * Reach; // I'm an idiot and forgot to MULTIPLY by the reach, as per the slide 5 minutes ago. sigh.
 	FVector LineTraceEnd = PlayerViewPointLocation + GrabReach;
 
-	// draw a red trace in the world to visualize
+	/// draw a red trace in the world to visualize
 	DrawDebugLine(
 		GetWorld(),
 		PlayerViewPointLocation,
@@ -63,7 +63,27 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	);
 
 
-	// raycast out to reach distance
-	// see what is hit
+	/// setup query parameters
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+	/// raycast out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+		);
+
+	/// see what is hit
+
+	// UE_LOG(LogTemp, Warning, TEXT("%s is being hit."), *Hit.GetActor()->GetName());			// so wrong it even crashes unreal on play
+
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit) {
+		UE_LOG(LogTemp, Warning, TEXT("%s is being hit."), *(ActorHit->GetName()));
+	}
+	
 }
 
